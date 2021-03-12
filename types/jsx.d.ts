46854cb0a78830ebdb732775ab1aba2aedd40610
hope <hope@i-on.net>
2021-03-12 11:21:45 +0900

    nuxt jsx setting

--- types/jsx.d.ts
+++ types/jsx.d.ts
@@ -4,6 +4,9 @@ declare global {
   namespace JSX {
     interface Element extends VNode {}
     interface ElementClass extends Vue {}
+    interface ElementAttributesProperty {
+      $props: {}
+    }
     interface IntrinsicElements {
       [elem: string]: any;
     }
