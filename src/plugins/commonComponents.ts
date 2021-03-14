1135ec431da4b0d12757664dba63cbaa5d0e4055
knightrun <to_promise@nate.com>
2021-03-14 20:15:52 +0900

    srcDir 설정 추가

--- src/plugins/commonComponents.ts
+++ src/plugins/commonComponents.ts
@@ -1,7 +1,7 @@
 import { Vue } from 'nuxt-property-decorator'
 
 import VCImage from '@/components/common/vcImage'
-import VCLink from '@/components/common/VCLink'
+import VCLink from '@/components/common/vcLink'
 
 Vue.component('vc-image', VCImage)
 Vue.component('vc-link', VCLink)
